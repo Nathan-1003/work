d71018135761ade2a3bd5054a573644ff20cd15c
Nathan-1003 <114478067+Nathan-1003@users.noreply.github.com>
2022-10-21 09:54:13 +0800

    Update 2.py

--- Case1/2.py
+++ Case1/2.py
@@ -1,5 +1,5 @@
 import pymysql  #pip3 install PyMySQL ,pip install mysql-connector-python
-import json
+import flask
 
 db = pymysql.connect(host='34.142.180.39',
                      port=13306,
@@ -8,51 +8,28 @@ db = pymysql.connect(host='34.142.180.39',
                      db = 'data_center',
                      charset='utf8')
 
-def select_version():
+#def test_select_channel():
 
-    cursor = db.cursor()
+sql_cmd = 'SELECT * FROM channel WHERE uuid="1_default" '
 
-    sql = 'SELECT VERSION()' #下SQL
+cur=db.cursor()
 
-    cursor.execute("SELECT VERSION()") #確認版本
+cur.execute(sql_cmd)
 
-    data = cursor.fetchone()
+result = cur.fetchall()
 
-    print (data)
+print(result)
 
-
-def select_channel():
-
-    cursor = db.cursor()
-
-    sql_cmd = 'SELECT uuid FROM data_center.channel'
-
-    cursor.execute("SELECT uuid FROM data_center.channel")
-
-    channel = cursor.fetchone()
-
-    print(channel)
-
-def test_select_channel():
-
-    sql_cmd = 'SELECT * FROM channel WHERE uuid="1_default" '
-
-    cur=db.cursor()
-
-    cur.execute(sql_cmd)
-
-    result = cur.fetchall()
-
-    print(result)
+str_result = ''.join(str(result).strip()) #轉換字串 將result tupl轉為str
 
 
+app = flask.Flask(__name__)
+@app.route("/")
+
+def hello():
+    return(str_result)
+
+if __name__ == '__main__':
+    app.run()
 
 
-
-
-
-
-if __name__== '__main__':
-    #select_version()
-    #select_channel()
-    test_select_channel()
