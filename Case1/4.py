c65726fcf3609970e4ef07a4ff7f1c7611f8da7b
Nathan-1003 <114478067+Nathan-1003@users.noreply.github.com>
2022-11-16 21:52:48 +0800

    Update 4.py

--- Case1/4.py
+++ Case1/4.py
@@ -1,13 +1,18 @@
-from dash import Dash ,dash_table  #pip install dash
-import pandas as pd #pip install pandas
-import dash_bootstrap_components as dbc
+from dash import Dash, dash_table
+import pandas as pd
 
-#db_connection = 'mysql+pymysql://root:oh_my_ody!@34.142.180.39:13306/data_center'
-db_connection = 'mysql+pymysql://default:oh_my_ody!@34.142.180.39:18123/data_center'
-#db_connection_str = 'mysql+pymysql://mysql_user:mysql_password@mysql_host/mysql_db'
+db_connection_str = 'mysql+pymysql://root:oh_my_ody!@34.142.180.39:13306/data_center'
 
-sql_cash_10 = pd.read_sql('SELECT * FROM data_center.cash LIMIT 10', con=db_connection)
-#sql_channel_id = pd.read_sql('SELECT channel_id,count(*) AS count FROM data_center.channel GROUP BY channel_id ORDER BY channel_id', con=db_connection)
+#db_connection = create_engine(db_connection_str)
+#df = pd.read_sql('SELECT channel_id FROM data_center.channel GROUP BY channel_id ORDER BY channel_id', con=db_connection_str)
+df = pd.read_sql('SELECT channel_id,count(*) AS count FROM data_center.channel GROUP BY channel_id ORDER BY channel_id', con=db_connection_str)
 
-print(sql_cash_10)
+print(type(df))
+#print(df)
 
+app = Dash(__name__)
+
+app.layout = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
