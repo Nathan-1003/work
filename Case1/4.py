c79b84cd0b662ce167568981599f5d7e514fa6d3
Nathan-1003 <114478067+Nathan-1003@users.noreply.github.com>
2022-11-16 21:53:37 +0800

    Update 4.py

--- Case1/4.py
+++ Case1/4.py
@@ -4,8 +4,8 @@ import pandas as pd
 db_connection_str = 'mysql+pymysql://root:oh_my_ody!@34.142.180.39:13306/data_center'
 
 #db_connection = create_engine(db_connection_str)
-#df = pd.read_sql('SELECT channel_id FROM data_center.channel GROUP BY channel_id ORDER BY channel_id', con=db_connection_str)
-df = pd.read_sql('SELECT channel_id,count(*) AS count FROM data_center.channel GROUP BY channel_id ORDER BY channel_id', con=db_connection_str)
+df = pd.read_sql('SELECT channel_id FROM data_center.channel GROUP BY channel_id ORDER BY channel_id', con=db_connection_str)
+#df = pd.read_sql('SELECT channel_id,count(*) AS count FROM data_center.channel GROUP BY channel_id ORDER BY channel_id', con=db_connection_str)
 
 print(type(df))
 #print(df)
