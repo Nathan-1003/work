0f01f437c8532689c8f6f212fbf15a3d9163d06c
Nathan-1003 <114478067+Nathan-1003@users.noreply.github.com>
2022-12-28 16:15:54 +0800

    kenn test

---
+++ work_Nathan/4test.py
@@ -0,0 +1,64 @@
+import json
+import re
+import pymysql  #pip3 install PyMySQL ,pip install mysql-connector-python
+import flask
+import datetime
+from flask import Flask, render_template
+from flask_cors import CORS
+
+db = pymysql.connect(host='34.142.180.39',
+                     port=13306,
+                     user='root',
+                     passwd='oh_my_ody!',
+                     db = 'data_center',
+                     charset='utf8')
+#def test_select_channel():
+
+#sql_cmd = 'SELECT channel_id FROM data_center.channel GROUP BY channel_id ORDER BY channel_id'
+sql_cmd = 'SELECT * FROM data_center.channel LIMIT 5 '
+cur=db.cursor()
+cur.execute(sql_cmd)#執行sql
+# result = cur.fetchall()#取出返回的結果值_tuple
+headers = [description[0] for description in cur.description]# 取得表的標題
+#print(headers)
+results = cur.fetchall()#查詢結果
+#print(results)
+data = [dict(zip(headers, row)) for row in results]#將表的標題和查詢結果組合成字典
+print(data)
+
+# for key,value in data[0].items():
+#     print('{key}:{value}'.format(key=key, value=value))
+
+cur.close()
+
+app = flask.Flask(__name__)
+CORS(app)#增加header
+
+@app.route("/")
+def hello():
+    return(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4999)# 更改port
+
+
+
+
+# print()
+# for row in cur:
+#     print(row)
+# cur.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
